afbff9d45bdf56be053874508a090628285b1caa
tbor <btimoteo@outlook.com>
2020-05-18 03:54:27 -0300

    Formularios de registros

--- mdt/login.js
+++ mdt/login.js
@@ -5,3 +5,12 @@ function abrir() {
 function cerrar() {
     document.getElementById("login").style.display="none";
 }
+
+
+function register() {
+    document.getElementById("registro").style.display="block";
+}
+
+function none() {
+document.getElementById("registro").style.display="none";
+}
