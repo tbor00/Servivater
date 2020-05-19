de88a6e9741963f492f7999c84d1a23f63ef3ae5
tbor <btimoteo@outlook.com>
2020-05-19 20:17:38 -0300

    cambios

--- mdt/js/login.js
+++ mdt/js/login.js
@@ -1,3 +1,12 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.casa', { delay: 800});
+ScrollReveal().reveal('.portafolio', { delay: 800 });
+ScrollReveal().reveal('.banner', { delay: 800 });
+ScrollReveal().reveal('.qs', { delay: 800 });
+
+
+
+
 function abrir() {
     document.getElementById("login").style.display="block";
 }
@@ -14,3 +23,7 @@ function register() {
 function none() {
 document.getElementById("registro").style.display="none";
 }
+
+
+
+
