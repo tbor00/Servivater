c2513ea2e76ea9387c4112e1fcfea21254ea67e3
tbor <btimoteo@outlook.com>
2020-06-06 15:37:04 -0300

    Cambios servivater

---
+++ servivater/js/login.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.casa', { delay: 800});
+ScrollReveal().reveal('.portafolio', { delay: 800 });
+ScrollReveal().reveal('.banner', { delay: 800 });
+ScrollReveal().reveal('.qs', { delay: 800 });
+
+
+
+
+function abrir() {
+    document.getElementById("login").style.display="block";
+}
+
+function cerrar() {
+    document.getElementById("login").style.display="none";
+}
+
+
+function register() {
+    document.getElementById("registro").style.display="block";
+}
+
+function none() {
+document.getElementById("registro").style.display="none";
+}
+
+
+
+
